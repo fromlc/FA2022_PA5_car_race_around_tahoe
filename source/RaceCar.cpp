bf2473a0f8d70295e9eb696e691a2450622cf9d7
L Carver <wifihum@gmail.com>
2022-10-11 14:20:37 -0700

    add reset() method ro RaceCar class

--- source/RaceCar.cpp
+++ source/RaceCar.cpp
@@ -26,9 +26,7 @@ RaceCar::RaceCar(const string& year, const string& make, const string& model) :
 	Car(year, make, model) {
 
 	srand((unsigned int) time(0));
-	speed = rand() % (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
-	speedLog.push_back(speed);
-	crash = false;
+	reset();
 }
 
 //------------------------------------------------------------------------------
@@ -56,8 +54,10 @@ void RaceCar::setSpeed(int speed) {
 // returns miles actually driven given fuel remaining
 //------------------------------------------------------------------------------
 int RaceCar::setRandomSpeed(int minSpeed, int maxSpeed) {
-	speed = rand() % (maxSpeed - minSpeed) + minSpeed;
+	int range = maxSpeed - minSpeed + 1;
+	speed = rand() % range + minSpeed;
 	speedLog.push_back(speed);
+
 	return speed;
 }
 
@@ -93,3 +93,14 @@ float RaceCar::getAverageSpeed() {
 	return (float) accumulateSpeed / (float) speedLog.size();
 }
 
+//------------------------------------------------------------------------------
+// resets car data
+//------------------------------------------------------------------------------
+void RaceCar::reset() {
+	odo.reset();
+	crash = false;
+
+	speed = rand() % (MAX_SPEED - MIN_SPEED + 1) + MIN_SPEED;
+	speedLog.clear();
+	speedLog.push_back(speed);
+}
